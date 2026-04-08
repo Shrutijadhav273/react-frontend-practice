b5e8d6d29be8d2dc1150eda23eec166ef52c9498
Shrutijadhav273 <shrutijadhav969@gmail.com>
2026-04-08 16:23:44 +0530

    button added in learning page for next page

--- app/(tabs)/learning.tsx
+++ app/(tabs)/learning.tsx
@@ -1,17 +1,18 @@
-import { StyleSheet, Text, View ,Pressable,Button} from "react-native";
 import { router } from "expo-router";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 export default function learning() {
     return (
         <View style={styles.container}>
              <Text style={styles.title}>Learning React Native</Text>
+             
         
 
         <Pressable
-                  style={styles.button}
-                  onPress={() => router.push("/learning")}
-                >
-                  <Text style={styles.buttonText}>Go to form validation</Text>
-                </Pressable>
+        style={styles.button}
+        onPress={() => router.push("/validation")}
+      >
+        <Text style={styles.buttonText}>Go to form validation</Text>
+      </Pressable>
                 </View>
     )
 }
@@ -26,10 +27,14 @@ const styles=StyleSheet.create({
         fontSize:22,
         color:"pink",
         fontWeight: "bold",
-    }
+    },
     button:{
         backgroundColor: "#6200EE",
         paddingVertical: 15,
         paddingHorizontal: 30,
+    },
+    buttonText:{
+        color: "#fff",
+        fontWeight: "bold",
     }
 });
