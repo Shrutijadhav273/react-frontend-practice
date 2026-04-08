07f1438d5a9bac0645ca25f52dfbc2e08cb8db2d
Shrutijadhav273 <shrutijadhav969@gmail.com>
2026-04-08 16:06:18 +0530

    learning page updated with text colour

--- app/(tabs)/learning.tsx
+++ app/(tabs)/learning.tsx
@@ -1,40 +1,19 @@
 import { StyleSheet, Text, View } from "react-native";
 
-export default function LearningScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Learning UI Design</Text>
-
-      <View style={styles.card}>
-        <Text style={styles.cardText}>• Flexbox Layout</Text>
-        <Text style={styles.cardText}>• Colors & Themes</Text>
-        <Text style={styles.cardText}>• Buttons & Inputs</Text>
-        <Text style={styles.cardText}>• Responsive Design</Text>
-      </View>
-    </View>
-  );
+export default function learning() {
+    return (
+        <View>
+            <Text>Learning page</Text>
+        </View>
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#121212",
-    padding: 20,
-  },
-  title: {
-    color: "#fff",
-    fontSize: 22,
-    fontWeight: "bold",
-    marginBottom: 20,
-  },
-  card: {
-    backgroundColor: "#1E1E1E",
-    padding: 20,
-    borderRadius: 15,
-  },
-  cardText: {
-    color: "#fff",
-    fontSize: 16,
-    marginBottom: 10,
-  },
+const styles=StyleSheet.create({
+    Text:{
+        fontSize:22,
+        color:"pink",
+    }
+
+
+
 });
